8ea51c199ee4ca8d6b6e7994b14668e542506b47
RuslanKhabibullinFs <ruslan.khabibullin@flatstack.com>
2017-08-13 15:31:28 +0500

    add sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,16 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "pdp-subdomains.herokuapp.com" }
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.sendgrid.net",
+    port:                 "587",
+    authentication:       :plain,
+    user_name:            ENV["SENDGRID_USERNAME"],
+    password:             ENV["SENDGRID_PASSWORD"],
+    enable_starttls_auto: true
+  }
 
   config.i18n.fallbacks = true
 
