41b0b562fc63ba0e235f75476ba82c604dc9af68
RuslanKhabibullinFs <ruslan.khabibullin@flatstack.com>
2017-09-04 11:48:16 +0300

    fix production domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 Rails.application.config.session_store :cookie_store, key: "_subdomain_session", domain: {
-  production: ".example.com",
+  production: ".ruslan-project.tk",
   development: ".lvh.me"
 }.fetch(Rails.env.to_sym, :all)
