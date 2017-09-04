e70741fc68ef403fa608d443071118f532c5f25d
RuslanKhabibullinFs <ruslan.khabibullin@flatstack.com>
2017-09-04 16:00:18 +0300

    remove session_store file

--- config/initializers/session_store.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.session_store :cookie_store, key: "_subdomain_session", domain: {
-  production: ".ruslan-project.tk",
-  development: ".lvh.me"
-}.fetch(Rails.env.to_sym, :all)
