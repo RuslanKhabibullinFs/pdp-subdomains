4383401e91afce43d7aebfd841a3da49ededf585
Ruslan Khabibullin <ruslan.khabibullin@flatstack.com>
2017-09-20 00:15:15 +0300

    move search to filter query (#17)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,12 @@
 class UsersController < ApplicationController
-  expose :filtered_users, -> { UsersFilterQuery.new(current_company.users, filter_params).all }
-  expose_decorated :users, -> { founded_users }
+  expose_decorated :users, -> { UsersFilterQuery.new(current_company.users, filter_params).all }
 
   def index
   end
 
   private
 
-  def founded_users
-    @founded_users ||= params[:search].present? ? filtered_users.search(params[:search]) : filtered_users
-  end
-
   def filter_params
-    params.permit(:filter_by_rating, sort_by: %i[option order])
+    params.permit(:filter_by_rating, :search, sort_by: %i[option order])
   end
 end
