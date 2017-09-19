d1c9b6b4aae8964f14bf6a5e3aa88f8b3fb0bdce
RuslanKhabibullinFs <ruslan.khabibullin@flatstack.com>
2017-09-20 00:12:57 +0300

    move search to filter query

--- app/query_objects/users_filter_query.rb
+++ app/query_objects/users_filter_query.rb
@@ -1,12 +1,28 @@
-class UsersFilterQuery < BaseFilterQuery
-  ALLOWED_PARAMS = %w[filter_by_rating sort_by].freeze
+class UsersFilterQuery
+  ALLOWED_PARAMS = %w[filter_by_rating search sort_by].freeze
   ALLOWED_SORT_PARAMS = %w[rating posts].freeze
   DEFAULT_ORDER_DIRECTION = :desc
 
+  attr_reader :relation, :filter_params
+  private :relation, :filter_params
+
+  def initialize(relation = User.all, filter_params = {})
+    @relation = relation
+    @filter_params = filter_params
+  end
+
+  def all
+    return sort_by_rating(relation, :desc) if sliced_params.empty?
+
+    sliced_params.reduce(relation) do |relation, (key, value)|
+      send(key, relation, value)
+    end
+  end
+
   private
 
-  def default_filter(relation)
-    sort_by_rating(relation, :desc)
+  def sliced_params
+    filter_params.slice(*ALLOWED_PARAMS).to_h
   end
 
   def filter_by_rating(relation, rating)
@@ -14,16 +30,21 @@ class UsersFilterQuery < BaseFilterQuery
     relation.where(rating: rating)
   end
 
+  def search(relation, query)
+    return relation if query.blank?
+    relation.search(query)
+  end
+
   def sort_by(relation, params)
-    return relation unless ALLOWED_SORT_PARAMS.include? params[:option]
-    send("sort_by_#{params[:option]}", relation, params[:order] || DEFAULT_ORDER_DIRECTION)
+    return relation unless ALLOWED_SORT_PARAMS.include? params["option"]
+    send("sort_by_#{params['option']}", relation, params["order"] || DEFAULT_ORDER_DIRECTION)
   end
 
-  def sort_by_rating(relation, order)
-    relation.order(rating: order)
+  def sort_by_rating(relation, direction)
+    relation.order(rating: direction)
   end
 
-  def sort_by_posts(relation, order)
-    relation.order(posts_count: order, rating: :desc)
+  def sort_by_posts(relation, direction)
+    relation.order(posts_count: direction, rating: :desc)
   end
 end
